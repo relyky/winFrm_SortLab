6c6d675eccd5e49aa46eeba3b311bf68e1f08379
relyky <relyky@hotmail.com>
2013-12-23 15:55:47 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace winFrm_SortLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numSleepTimespan = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numSleepTimespan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(393, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1插入排序法";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(34, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(470, 446);
+            this.panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(393, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2泡沫排序法";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numSleepTimespan
+            // 
+            this.numSleepTimespan.Location = new System.Drawing.Point(131, 15);
+            this.numSleepTimespan.Name = "numSleepTimespan";
+            this.numSleepTimespan.Size = new System.Drawing.Size(90, 22);
+            this.numSleepTimespan.TabIndex = 3;
+            this.numSleepTimespan.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numSleepTimespan.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numSleepTimespan.ValueChanged += new System.EventHandler(this.numSleepTimespan_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sleep Timespan";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3選擇排序法";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "比較次數：？？？";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "交換次數：？？？";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 595);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numSleepTimespan);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numSleepTimespan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numSleepTimespan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace winFrm_SortLab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Graphics g = this.CreateGraphics();
+            //Pen myPen = new Pen(Color.Black, 1);
+            //Point sp = new Point(0, 0);//starting point sp
+            //Point ep = new Point(5, 5);//ending point ep
+
+            //g = this.CreateGraphics();//tells compiler that we are going to draw on this very form
+            //g.DrawLine(myPen, sp, ep);
+
+            //g.DrawEllipse(myPen, 20, 30, 90, 30);
+
+            //..................................
+            //// draw 2D dot array
+            //for (int x = 100; x < 520; x += 10)
+            //    for (int y = 100; y < 520; y += 10)
+            //        e.Graphics.DrawRectangle(Pens.Black, x, y, 1,1);
+
+
+            ////# 設定座標系統
+            ////Graphics g = e.Graphics;
+            //Graphics g = panel1.CreateGraphics();
+            //g.TranslateTransform(30, panel1.Height - 30); // 變更原點
+            //g.ScaleTransform(1.0f, -1.0f); // 變更方向
+            //// 
+            //g.DrawLine(Pens.Black, -30, 0, panel1.Width, 0);
+            //g.DrawLine(Pens.Black, 0, -30, 0, panel1.Height);
+
+            //// draw dot array
+            //Point[] dots = new Point[100];
+            //for (int i = 0; i < 100; i++)
+            //    dots[i] = new Point( 5 * i, 5 * i);
+
+            //foreach (Point dot in dots)
+            //    g.DrawRectangle(Pens.Blue, dot.X, dot.Y, 1, 1);
+
+            //// 變更座標系統
+            ////g.PageUnit = GraphicsUnit.Pixel;
+            ////g.PageScale = -1;
+            ////g.ScaleTransform(1F, -1F);
+
+            //foreach (Point dot in dots)
+            //    g.DrawRectangle(Pens.Red, dot.X, dot.Y, 1, 1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //# 設定座標系統
+            //Graphics g = e.Graphics;
+            Graphics g = panel1.CreateGraphics();
+            g.TranslateTransform(30, panel1.Height - 30); // 變更原點
+            g.ScaleTransform(1.0f, -1.0f); // 變更方向
+            // 
+            g.DrawLine(Pens.Black, -30, 0, panel1.Width, 0);
+            g.DrawLine(Pens.Black, 0, -30, 0, panel1.Height);
+            //
+            g.ScaleTransform(4.0f, 4.0f); // 放大４倍
+
+            //# init. int array
+            int[] datas = new int[100];
+            for (int i = 0; i < 100; i++)
+                datas[i] = i;
+
+            // randomize
+            Random r = new Random();
+            r.Next(100);
+            for (int i = 0; i < 100; i++)
+            {
+                int i1 = r.Next(100);
+                int i2 = r.Next(100);
+                // swap
+                int tmp = datas[i1];
+                datas[i1] = datas[i2];
+                datas[i2] = tmp;
+            }
+
+            // draw
+            DrawDatas(datas, g, Pens.Blue);
+            // sort -------------------
+            InsertionSort(datas, g, Pens.Blue, this);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //# 設定座標系統
+            //Graphics g = e.Graphics;
+            Graphics g = panel1.CreateGraphics();
+            g.TranslateTransform(30, panel1.Height - 30); // 變更原點
+            g.ScaleTransform(1.0f, -1.0f); // 變更方向
+            // 
+            g.DrawLine(Pens.Black, -30, 0, panel1.Width, 0);
+            g.DrawLine(Pens.Black, 0, -30, 0, panel1.Height);
+            //
+            g.ScaleTransform(4.0f, 4.0f); // 放大４倍
+
+            //# init. int array
+            int[] datas = new int[100];
+            for (int i = 0; i < 100; i++)
+                datas[i] = i;
+
+            // randomize
+            Random r = new Random();
+            r.Next(100);
+            for (int i = 0; i < 100; i++)
+            {
+                int i1 = r.Next(100);
+                int i2 = r.Next(100);
+                // swap
+                int tmp = datas[i1];
+                datas[i1] = datas[i2];
+                datas[i2] = tmp;
+            }
+
+            // draw
+            DrawDatas(datas, g, Pens.Blue);
+            // sort -------------------
+            BubbleSort(datas, g, Pens.Aqua, this);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //# 設定座標系統
+            //Graphics g = e.Graphics;
+            Graphics g = panel1.CreateGraphics();
+            g.TranslateTransform(30, panel1.Height - 30); // 變更原點
+            g.ScaleTransform(1.0f, -1.0f); // 變更方向
+            // 
+            g.DrawLine(Pens.Black, -30, 0, panel1.Width, 0);
+            g.DrawLine(Pens.Black, 0, -30, 0, panel1.Height);
+            //
+            g.ScaleTransform(4.0f, 4.0f); // 放大４倍
+
+            //# init. int array
+            int[] datas = new int[100];
+            for (int i = 0; i < 100; i++)
+                datas[i] = i;
+
+            // randomize
+            Random r = new Random();
+            r.Next(100);
+            for (int i = 0; i < 100; i++)
+            {
+                int i1 = r.Next(100);
+                int i2 = r.Next(100);
+                // swap
+                int tmp = datas[i1];
+                datas[i1] = datas[i2];
+                datas[i2] = tmp;
+            }
+
+            // draw
+            DrawDatas(datas, g, Pens.Blue);
+            // sort -------------------
+            SelectionSort(datas, g, Pens.Cyan, this);
+        }
+
+        #region tools
+
+        // 插入排序法
+        static protected void InsertionSort(int[] datas, Graphics g, Pen pen, Form1 mainForm)
+        {
+            //從未排序數列取出一元素。
+            //由後往前和已排序數列元素比較，直到遇到不大於自己的元素並插入此元素之後；若都沒有則插入在最前面。
+            //重複以上動作直到未排序數列全部處理完成。
+
+            int[] sortedDatas = new int[100];
+            for (int i = 0; i < datas.Length; i++)
+            {
+                // 取出
+                int c = datas[i];
+                g.DrawRectangle(Pens.LightGray, i, datas[i], 1, 1);
+
+                // 比較
+                int insertIndex = i;
+                for (; insertIndex > 0 && sortedDatas[insertIndex - 1] > c; insertIndex--) ; // compare
+
+                // 插入-shift
+                for (int j = i; j > insertIndex; j--)
+                {
+                    g.DrawRectangle(Pens.LightGray, j-1, sortedDatas[j-1], 1, 1);
+                    sortedDatas[j] = sortedDatas[j - 1];
+                    g.DrawRectangle(Pens.Cyan, j, sortedDatas[j], 1, 1);
+
+                    // wait a short time
+                    System.Threading.Thread.Sleep((int)mainForm.numSleepTimespan.Value);
+                }
+
+                // 插入-new
+                sortedDatas[insertIndex] = c;
+                g.DrawRectangle(Pens.Green, insertIndex, sortedDatas[insertIndex], 1, 1);
+            }
+
+            // 
+            //DrawDatas(sortedDatas, g, Pens.Pink);
+        }
+
+        // 泡沫排序法
+        static protected void BubbleSort(int[] datas, Graphics g, Pen pen, Form1 mainForm)
+        {
+            for (int i = 0; i < datas.Length - 1; i++)
+                for (int j = i + 1; j < datas.Length; j++)
+                    if (datas[i] > datas[j]) // compare
+                    {
+                        // before swap
+                        g.DrawRectangle(Pens.LightGray, i, datas[i], 1, 1);
+                        g.DrawRectangle(Pens.LightGray, j, datas[j], 1, 1);
+
+                        // swap
+                        int tmp = datas[i];
+                        datas[i] = datas[j];
+                        datas[j] = tmp;
+                        //
+                        //DrawDatas(datas, g, pen);
+                        g.DrawRectangle(pen, i, datas[i], 1, 1);
+                        g.DrawRectangle(pen, j, datas[j], 1, 1);
+
+                        // wait a short time
+                        System.Threading.Thread.Sleep((int)mainForm.numSleepTimespan.Value);
+                    }
+        }
+
+        // 選擇排序法
+        static protected void SelectionSort(int[] datas, Graphics g, Pen pen, Form1 mainForm)
+        {
+            //Function selectionSort(Type data[1..n])
+            //    Index i, j, max  --- or min
+            //    For i from 1 to n do
+            //        max = i
+            //        For j from i + 1 to n do
+            //            If data[j] > data[max] then
+            //                max = j
+            //
+            //        Exchange data[i] and data[max]
+            //End
+
+            //# Index i, j, max  --- or min
+            int minIndex;
+            for (int i = 0; i < datas.Length; i++)
+            {
+                minIndex = i;
+                for(int j = i+1; j < datas.Length; j++)
+                    if(datas[j] < datas[minIndex]) // compare
+                        minIndex = j;
+
+                //# Exchange data[i] and data[max]
+                // before swap
+                g.DrawRectangle(Pens.LightGray, i, datas[i], 1, 1);
+                g.DrawRectangle(Pens.LightGray, minIndex, datas[minIndex], 1, 1);
+
+                // swap
+                int tmp = datas[i];
+                datas[i] = datas[minIndex];
+                datas[minIndex] = tmp;
+                
+                //DrawDatas(datas, g, pen);
+                g.DrawRectangle(pen, i, datas[i], 1, 1);
+                g.DrawRectangle(pen, minIndex, datas[minIndex], 1, 1);
+
+                // wait a short time
+                System.Threading.Thread.Sleep((int)mainForm.numSleepTimespan.Value);
+            }
+
+            // 
+            //DrawDatas(sortedDatas, g, Pens.Pink);
+        }
+
+        static protected void DrawDatas(int[] datas, Graphics g, Pen pen)
+        {
+            //Bitmap bmp = new Bitmap(200, 200, g);
+
+            // draw
+            //g.DrawRectangle(0, 0, 100, 100);
+            g.FillRectangle(Brushes.LightGray, 0, 0, 100, 100);
+            for (int i = 0; i < 100; i++)
+                g.DrawRectangle(pen, i, datas[i], 1, 1);
+        }
+
+        #endregion
+
+        private void numSleepTimespan_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
